4dda471b0c3c87085b68b98d565677eadcac6ee0
Emanuele Bordoni <emanuele.bordoni@intesys.it>
2026-04-24 14:30:50 +0200

    fix(addon): allowed_chat_ids come stringa separata da virgola invece di lista int

--- hassio-addon/classeviva-bot/run.sh
+++ hassio-addon/classeviva-bot/run.sh
@@ -13,8 +13,8 @@ TELEGRAM_TOKEN=$(jq -r '.telegram_token // empty' "$OPTIONS")
 AI_PROVIDER=$(jq -r '.ai_provider // "openai"' "$OPTIONS")
 AI_MODEL=$(jq -r '.ai_model // empty' "$OPTIONS")
 AI_API_KEY=$(jq -r '.ai_api_key // empty' "$OPTIONS")
-# Lista di chat ID autorizzati, separati da virgola (es. "123456,789012")
-ALLOWED_CHAT_IDS=$(jq -r '[.allowed_chat_ids // [] | .[] | tostring] | join(",")' "$OPTIONS")
+# Chat ID autorizzati come stringa separata da virgola (es. "123456,789012")
+ALLOWED_CHAT_IDS=$(jq -r '.allowed_chat_ids // ""' "$OPTIONS")
 
 if [ -z "$TELEGRAM_TOKEN" ]; then
   echo "[classeviva-bot] ERRORE: telegram_token non impostato!"
